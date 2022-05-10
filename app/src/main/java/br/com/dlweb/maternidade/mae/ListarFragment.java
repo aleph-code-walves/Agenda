ccfa5d53cc6d4edf0d142534be7a7782e440c199
Lucas Debatin <lucasdebatin@univali.br>
2022-05-09 21:18:11 -0300

    CM - persistência de dados - Mãe

--- app/src/main/java/br/com/dlweb/maternidade/mae/ListarFragment.java
+++ app/src/main/java/br/com/dlweb/maternidade/mae/ListarFragment.java
@@ -3,12 +3,17 @@ package br.com.dlweb.maternidade.mae;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import br.com.dlweb.maternidade.R;
+import br.com.dlweb.maternidade.database.DatabaseHelper;
 
 public class ListarFragment extends Fragment {
 
@@ -24,6 +29,25 @@ public class ListarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.mae_fragment_listar, container, false);
+        View v = inflater.inflate(R.layout.mae_fragment_listar, container, false);
+        DatabaseHelper databaseHelper = new DatabaseHelper(getActivity());
+        ListView lv = v.findViewById(R.id.listViewMaes);
+        databaseHelper.getAllMae(getActivity(), lv);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                TextView tvId = view.findViewById(R.id.textViewIdListMae);
+                Bundle b = new Bundle();
+                b.putInt("id", Integer.parseInt(tvId.getText().toString()));
+
+                EditarFragment editarFragment = new EditarFragment();
+                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+                editarFragment.setArguments(b);
+                ft.replace(R.id.frameMae, editarFragment).commit();
+            }
+        });
+
+        return v;
     }
 }
